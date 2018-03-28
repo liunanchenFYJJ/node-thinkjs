cebf358f98764d499750adba76ac98e49886827e
liunanchenFYJJ <liunan_chen@163.com>
2018-03-28 15:37:55 +0800

    update brfore handle model

--- src/common/model/user.js
+++ src/common/model/user.js
@@ -3,5 +3,11 @@
  * model
  */
 export default class extends think.model.base {
-
+    // schema = {
+    //     create_date: {
+    //         default: () => {
+    //             return moment().format('YYYY-MM-DD')
+    //         }
+    //     }
+    // }
 }
