b0850b194fb9752e364c62588736d7d9d4bf3c0e
liunanchenFYJJ <liunan_chen@163.com>
2018-03-29 17:57:44 +0800

    add module admin

---
+++ www/static/js/vue_echarts.js
@@ -0,0 +1,41 @@
+(function () {
+    layui.use(['layer'], function () {
+        var layer = layui.layer;
+        var events = window.eventDispatcher;
+        var mqtt = null;
+        var timer = null;
+        var vueMode = {
+            prjData: [{
+                prjName: '普洛斯20kV变电站',
+                cropName: '苏州普洛斯物流园',
+                cropType: '服务行业',
+                kVa: 1260,
+                ckW: 314.56,
+                maxkW: 513.48,
+                runStatus: 1
+            }]
+        };
+        //vue 实例
+        var app1 = new Vue({
+            el: '#vue_app',
+            data: vueMode,
+
+            filters: {
+                time: function (value, formatString) {
+                    formatString = formatString || 'YYYY-MM-DD HH:mm:ss';
+                    return moment(value).format(formatString);
+                }
+            },
+            methods: {
+
+                sensorHistory: function (data) {},
+                mydel: function (data) {},
+                kpiCheck: function (item) {
+                    return 'run'
+                }
+
+            }
+        });
+
+    });
+}());
