ea6efbb87699d00322e4cdaf1605c7405b0f8106
liunanchenFYJJ <liunan_chen@163.com>
2018-04-10 17:53:10 +0800

    update company

--- src/admin/controller/company.js
+++ src/admin/controller/company.js
@@ -7,8 +7,31 @@ export default class extends Base {
    * index action
    * @return {Promise} []
    */
-  indexAction(){
-    //auto render template file index_index.html
+  // companyList
+  async indexAction() {
     return this.display();
+  }
+
+  async getcompaysAction() {
+    let model = this.model('companys');
+    let data = await model.select();
+    return this.success(data);
+  }
+
+  async getcompayspageAction() {
+    let page = this.post('page') || 1;
+    let limit = this.post("limit") || 10;
+
+    let model = this.model('companys');
+    let data = await model.page(page, limit).countSelect();
+    return this.json({code:0,msg:"",count:data.count,data:data.data});
+  }
+
+  // addCompany
+  async addAction() {
+    let model = this.model('companys');
+    let company = this.post();
+    console.log(company);
+    let insertId = await model.add(company);
   }
 }
