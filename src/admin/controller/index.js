9f471398768b32c873e7f2b953309b86ece40ccd
liunan <liunan@promote.cache-dns.local>
2018-04-11 23:03:17 +0800

    add login&logout

--- src/admin/controller/index.js
+++ src/admin/controller/index.js
@@ -7,13 +7,41 @@ export default class extends Base {
    * index action
    * @return {Promise} []
    */
-  indexAction(){
+  indexAction() {
     //auto render template file index_index.html
     return this.display();
   }
 
-  echartsAction(){
+  echartsAction() {
     //auto render template file index_index.html
     return this.display();
+  }
+
+  async loginAction() {
+    var self = this;
+    if (self.isPost()) {
+      let username = self.post('username');
+      let password = self.post('password');
+      let model = await this.model('userinfo');
+      return model.where({
+        username: username,
+        password: password
+      }).find().then(function (data) {
+        if (isEmpty(data)) {
+          return self.error(403, '用户或者密码不正确');
+        } else {
+          return self.session('userinfo', data);
+        }
+      }).then(function () {
+        return self.redirect('index');
+      })
+    }
+    return self.display();
+
+  }
+
+  async logoutAction() {
+    await this.session();
+    return this.redirect('login');
   }
 }
