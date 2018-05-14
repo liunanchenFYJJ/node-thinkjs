b8565b606857d9626b42c8ac5eaa606f2f0bea8a
liunanchenFYJJ <liunan_chen@163.com>
2018-05-14 18:00:01 +0800

    update designer

---
+++ src/designer/controller/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+import Base from './base.js';
+
+export default class extends Base {
+  /**
+   * index action
+   * @return {Promise} []
+   */
+  indexAction(){
+    //auto render template file index_index.html
+    return this.display();
+  }
+
+  async viewAction()
+  {
+    return this.display();
+  }
+
+  async getchartcontentAction()
+  {
+    let id = this.get('query');
+    let mo = this.model('hz_uviews');
+    let data = await mo.where({id:id}).find();
+    if(!data)
+    {
+     return this.fail(200,"无法找到");
+    }
+    else
+    {
+         //console.log(data);
+        return this.success(data.content);
+    }
+
+    
+   
+
+  }
+
+  async svgdesignerAction(){
+    return this.display();
+  }
+
+
+  async svgAction()
+  {
+    return this.display();
+  }
+
+  async leafletAction()
+  {
+    return this.display();
+  }
+}
