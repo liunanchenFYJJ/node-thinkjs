e709dbfb1ff84d6aa4cbc91759e811ced4a43d91
liunanchenFYJJ <liunan_chen@163.com>
2018-05-23 17:27:38 +0800

    update upload pic

--- src/designer/controller/index.js
+++ src/designer/controller/index.js
@@ -7,48 +7,56 @@ export default class extends Base {
    * index action
    * @return {Promise} []
    */
-  indexAction(){
+  indexAction() {
     //auto render template file index_index.html
     return this.display();
   }
 
-  async viewAction()
-  {
+  async viewAction() {
     return this.display();
   }
 
-  async getchartcontentAction()
-  {
+  async getchartcontentAction() {
     let id = this.get('query');
     let mo = this.model('hz_uviews');
-    let data = await mo.where({id:id}).find();
-    if(!data)
-    {
-     return this.fail(200,"无法找到");
+    let data = await mo.where({
+      id: id
+    }).find();
+    if (!data) {
+      return this.fail(200, "无法找到");
+    } else {
+      //console.log(data);
+      return this.success(data.content);
     }
-    else
-    {
-         //console.log(data);
-        return this.success(data.content);
-    }
 
-    
-   
+
+
 
   }
 
-  async svgdesignerAction(){
+  async svgdesignerAction() {
     return this.display();
   }
 
 
-  async svgAction()
-  {
+  async svgAction() {
     return this.display();
   }
 
-  async leafletAction()
-  {
+  async leafletAction() {
     return this.display();
+  }
+
+  async uploadAction() {
+    let model = this.model('pic');
+    let post = this.post();
+    console.log(post);    
+    let uploadId = await model.add({'vidPublicUrl': '/static/img/test.png'});
+    let data = await model.where({id: uploadId}).find();
+    return this.json({
+      code: 0,
+      msg: "success",
+      data: data
+    })
   }
 }
